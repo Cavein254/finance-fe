e956141eee07be8322d4d01272d2caae871cb6ce
johnmuthua <caveomuthama@gmail.com>
2025-01-03 14:39:58 +0300

    feat: add stock Item component ## Description
    Create a stockItem component that displays information of a given stock

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import SearchCmp from "@/components/common/SearchCmp";
 import Spacer from "@/components/common/Spacer";
 import Header from "@/components/header/Header";
 import StockList from "@/components/stock/StockList";
@@ -7,6 +8,7 @@ export default function Index() {
   return (
     <View>
       <Header />
+      <SearchCmp />
       <Spacer />
       <StockList />
     </View>
---
+++ components/common/SearchCmp.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchCmp = () => {
+  return (
+    <View>
+      <input type="text" placeholder="Search" />
+    </View>
+  )
+}
+
+export default SearchCmp
